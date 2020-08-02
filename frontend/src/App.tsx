ae8bb0da7ea3d2ce91ba65500546a0a083459082
Matt Barnfield <matt.barnfield@softwire.com>
2020-08-02 13:40:43 +0100

    Config for calling API functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get('/api/hello-world')
+        .then(response => setMessage(response.data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>{message}</p>
     </div>
   );
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
+const router = express.Router();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+router.get('/hello-world', (request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
-});
+})
+
+app.use('/api', router);
+
+export const api = functions
+    // hosting only supports rewrite rules to us-central1 https://firebase.google.com/docs/hosting/full-config#rewrite-functions
+    .region('us-central1')
+    .https
+    .onRequest(app as any);
